f4a6ac38aa44592378c1639e0bb242cd178f5d5b
Murilo Zanini de Carvalho <murilo@Mac-mini-de-Murilo.local>
2019-04-19 09:37:47 -0300

    1050 - if com varios elif para verificar numero inteiro

---
+++ Problemas/1050/main.py
@@ -0,0 +1,21 @@
+ddd = int(input())
+
+if ddd == 11:
+    saida = 'Sao Paulo'
+elif ddd == 61:
+    saida = 'Brasilia'
+elif ddd == 71:
+    saida = 'Salvador'
+elif ddd == 21:
+    saida = 'Rio de Janeiro'
+elif ddd == 32:
+    saida = 'Juiz de Fora'
+elif ddd == 19:
+    saida = 'Campinas'
+elif ddd == 27:
+    saida = 'Vitoria'
+elif ddd == 31:
+    saida = 'Belo Horizonte'
+else:
+    saida = 'DDD nao cadastrado'
+print(saida)
