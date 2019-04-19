f0b91644be7037d908a4cf3ee4fed1e12edf2a87
Murilo-ZC <carvalho.zanini@gmail.com>
2019-04-19 01:34:14 -0300

    1046 - Tempo de jogo

--- Problemas/1046/main.py
+++ Problemas/1046/main.py
@@ -1,4 +1,11 @@
 entrada = input().split()
-lados = []
+tempos = []
 for i in range(len(entrada)):
-    lados.append(float(entrada[i]))
+    tempos.append(int(entrada[i]))
+if len(set(tempos)) == 1:
+    saida = "O JOGO DUROU 24 HORA(S)"
+elif tempos[0] < tempos[1]:
+    saida = "O JOGO DUROU %i HORA(S)" % (tempos[1]-tempos[0])
+else:
+    saida = "O JOGO DUROU %i HORA(S)" % ((24-tempos[0])+tempos[1])
+print(saida)
